202c304b7edaab96f620139240fa3b80002425e9
kamran <gulnazpara1432@gmail.com>
2025-12-11 12:22:33 +0530

    Implemented Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-import logger
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -10,17 +9,13 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_meassage,error_detail:sys):
-        super().__init__(error_meassage)
-        self.error_message = error_message_detail(error_meassage,error_detail=error_detail)
+    def __init__(self, error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
     
 
 if __name__=="__main__":
-    try:
-        a =1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+    pass
